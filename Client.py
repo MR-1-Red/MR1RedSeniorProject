f86ab0423f4a8f2098b10922c7bbe719be64d352
Dillon Horton <djhorton2002@gmail.com>
2024-02-29 21:42:55 -0500

    added time tracking

--- Client.py
+++ Client.py
@@ -13,7 +13,7 @@ wrapped_socket = context.wrap_socket(s, server_hostname=hostname)
 #wrapped_socket.send("GET / HTTP/ ")
 
 kems = oqs.get_enabled_kem_mechanisms()
-pprint(kems, compact=True)
+
 # create client and server with sample KEM mechanisms
 with oqs.KeyEncapsulation(alg) as client:
     with oqs.KeyEncapsulation(alg) as server:
@@ -21,15 +21,25 @@ with oqs.KeyEncapsulation(alg) as client:
         pprint(client.details)
 
         # client generates its keypair
+        genStart=time.time()
         public_key_client = client.generate_keypair()
+        genEnd=time.time()
+        total = genEnd-genStart
+        print("Generation Time: "+str(total)+"s")
         
-
         # the server encapsulates its secret using the client's public key
+        enStart=time.time()
         ciphertext, shared_secret_server = server.encap_secret(public_key_client)
-
+        enEnd =time.time()
+        total = enEnd-enStart
+        print("Encryption time: "+str(total)+"s")
+        
         # the client decapsulates the server's ciphertext to obtain the shared secret
+        deStart=time.time()
         shared_secret_client = client.decap_secret(ciphertext)
-
+        deEnd=time.time()
+        total = deEnd-deStart
+        print("Decryption time: "+str(total)+"s")
         print("\nShared secretes coincide:", shared_secret_client == shared_secret_server)
 
         
